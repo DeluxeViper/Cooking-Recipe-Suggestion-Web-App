b492e8d9b9c58189bca2247e0f01a34f213b0c78
Dan Kotov <dkotov@ryerson.ca>
2022-10-03 21:40:03 -0400

    Initial helper package with a web page content getter

---
+++ recipe_parser/helpers/main.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_page(url: str):
+  page = None
+
+  try:
+    request = requests.get(url)
+    
+    if not request.status_code == 200:
+      print("Request response not OK")
+    
+    page = request.text 
+  except Exception as e:
+    print(f"Exception encountered when fetching page: {url}; e: {e}")
+
+  return page
