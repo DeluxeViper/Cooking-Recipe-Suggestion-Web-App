3b6b11332a51691c982b5a245407cc5ece646e5a
Dan Kotov <dkotov@ryerson.ca>
2022-10-25 21:17:20 -0400

    Adding steps parser

--- recipe_parser/pages/recipe/main.py
+++ recipe_parser/pages/recipe/main.py
@@ -2,14 +2,27 @@ from bs4 import BeautifulSoup
 from helpers import get_page
 
 from .ingredients import get_ingredients
+from .steps import get_cooking_steps
 
 
-def parse_recipe(url: str):
+def parse_recipe(url: str, cuisine: str):
     page = get_page(url)
     if not page:
         return None
 
     soup_page = BeautifulSoup(page, "lxml")
-    ingredients = get_ingredients(soup_page)
 
-    return ingredients
+    ingredients = get_ingredients(soup_page)
+    steps = get_cooking_steps(soup_page)
+
+    recipe = {
+        "name": "Recipe name",
+        "description": "Recipe description",
+        "cuisine": cuisine,
+        "img": "Recipe img",
+        "ingredients": ingredients,
+        "steps": steps,
+        "preparation_time": "Recipe preparation time",
+        "cooking_time": "Recipe cooking time",
+    }
+    return recipe
--- recipe_parser/parser.py
+++ recipe_parser/parser.py
@@ -4,8 +4,6 @@ import pages
 # * just a script to test whats possible right now, not final by any means
 # * nor does it follow ANY best practices
 
-f = open("ingredients.txt", "a", buffering=1)
-recipe_ingredients_set = set()
 
 # get links for each cuisine's homepage
 cuisine_page_links_list = pages.cuisines.get_cuisine_page_links()
@@ -17,18 +15,13 @@ for cuisine_page_link in cuisine_page_links_list:
     for recipe_link in cuisine_recipes_links_list:
         # try to extract ingredients from a recipe
         try:
-            recipe_ingredients = pages.recipe.parse_recipe(recipe_link)
+            recipe = pages.recipe.parse_recipe(recipe_link, "cuisine")
+            print(recipe, "\n")
         # if parsing is unsuccessfull, discard recipe
         except Exception as e:
             continue
 
-        for ingredient in recipe_ingredients:
-            if ingredient not in recipe_ingredients_set:
-                f.write(ingredient + "\n")
-            recipe_ingredients_set.add(ingredient)
 
-
-f.close()
 # print("N of recipes attempted: ", att_counter)  # 2100 +-
 # print("N of recipes parsed: ", counter)  # 1500 +-
 # print("N of ingredients attempted: ", ing_counter)  # 15000 +- attempted
