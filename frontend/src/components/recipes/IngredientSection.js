10fe155f610c98d80a1cbd5b449ad5e341cb5640
Mico Cataga <mcataga@ryerson.ca>
2022-11-29 15:13:20 -0500

    change to arrow functions

--- frontend/src/components/recipes/IngredientSection.js
+++ frontend/src/components/recipes/IngredientSection.js
@@ -3,19 +3,22 @@ import PropTypes from "prop-types";
 import { Divider, Typography } from "@material-ui/core";
 import StrikeThroughText from "../StrikeThroughText";
 
-IngredientSection.propTypes = {
-  //refactor when ingredientItem has ID
-  recipeAndIngredients: PropTypes.shape({
-    recipeName: PropTypes.string,
-    ingredientList: PropTypes.arrayOf(
-      PropTypes.shape({
-        ingredientName: PropTypes.string,
-      })
-    ),
-  }),
-};
 
-function IngredientSection(props) {
+
+const IngredientSection = (props) => {
+
+  IngredientSection.propTypes = {
+    //refactor when ingredientItem has ID
+    recipeAndIngredients: PropTypes.shape({
+      recipeName: PropTypes.string,
+      ingredientList: PropTypes.arrayOf(
+        PropTypes.shape({
+          ingredientName: PropTypes.string,
+        })
+      ),
+    }),
+  };
+
   let { recipeName, ingredientList } = props.recipeAndIngredients;
   return (
     <div>
